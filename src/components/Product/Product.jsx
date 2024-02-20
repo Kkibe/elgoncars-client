cc823f307263e67095165edf19db6281f9af5b46
Kkibe <137826199+Kkibe@users.noreply.github.com>
2024-02-20 09:35:56 +0300

    Update Product.jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,110 +1,20 @@
 import React from 'react';
 import './Product.scss';
+import { Rating } from '@mui/material';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 
-import TTA from '../../assets/TTA.png';
-import TY from "../../assets/TY.png";
-import Lc300 from "../../assets/lc300.png";
 
-import ToyotaLC from "../../assets/ToyotaLC.jpg";
-import FR12v from '../../assets/FR12v.jpg';
-import Loader from '../Loader/Loader';
-
-const Data = [
-            {
-                id:1,
-                name: "Smartchat",
-                title: "New 2019 Audi 54 Premium Plus",
-                img: TTA,
-                desc: "If you are a freelacer then we got your back Lorem ipsum dolor sit",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-            
-            {
-                id:2,
-                name: "Bigplay",
-                title: "Upcider for freelancers",
-                img: TY,
-                desc: "If you are a freelacer then we got your back Lorem ipsum dolor sit.",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-
-            {
-                id:3,
-                name: "Bigshare",
-                title: "Upcider for freelancers",
-                img: Lc300,
-                desc: "If you are a freelacer then we got your back Lorem ipsum dolor sit.",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-            
-            {
-                id:4,
-                name: "DefineAI",
-                title: "Upcider for freelancers",
-                img: ToyotaLC,
-                desc: "We bring you a freelancer platform where we connect skills with market.",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-            
-            {
-                id:5,
-                name: "Healthgen",
-                title: "Upcider for freelancers",
-                img: FR12v,
-                desc: "If you are a freelacer then we got your back Lorem ipsum dolor sit.",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-            
-            {
-                id:6,
-                name: 'Upcider',
-                title: "Upcider for freelancers",
-                img: FR12v,
-                desc: "If you are a freelacer then we got your back Lorem ipsum dolor sit.",
-                link: "#", 
-                feature: [
-                    'Diesel',
-                    '3346 cc',
-                    '304 bhp & 700 Nm'
-                ]
-            },
-        ]
-
-export default function Product() {
+export default function Product({item}) {
+const value = 2;
+  
   return (
-    <div className='products' >
-    {
-        Data.map(item => {
-            return <div className="card" id={item.id}>
+            <div className="product" id={item.id}>
                 <div className="image-container">
                     <img src={`${item.img}`} alt="" />
                 </div>
-                
+                {
+                    item.liked ? <Favorite className='liked-btn'/> : <FavoriteBorder className='like-btn'/>
+                }
                 <div className="content">
                     <h1>{item.title}</h1>
                     <p>
@@ -117,17 +27,14 @@ export default function Product() {
                 </div>
                 <div className="info">
                     <span>Toyota</span>
-                    <span>NEW</span>
+                    <span>USED</span>
                 </div>
+                <Rating name="read-only" value={value} readOnly />
                 <div className='btn-container'>
                     <button disabled>$5550</button>
                     <button>BUY NOW &raquo;</button>
                 </div>
                 
             </div>
-            
-        })
-    }
-</div>
   )
 }
