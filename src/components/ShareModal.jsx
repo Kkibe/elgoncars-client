6c458bd694d917fe17d8c723733511ee3dc31ef9
Kibet <137826199+Kkibe@users.noreply.github.com>
2025-03-04 12:23:09 +0300

    Create ShareModal.jsx

---
+++ src/components/ShareModal.jsx
@@ -0,0 +1,30 @@
+import { CloseOutlined, CopyAll, CopyAllOutlined, Facebook, FacebookOutlined, Instagram, LinkOutlined, Telegram, Twitter, WhatsApp } from '@mui/icons-material';
+import React from 'react';
+
+export default function ShareModal() {
+  return (
+    <div className='share-modal'>
+        <header>
+            <span>Share Modal</span>
+            <CloseOutlined className='close'/>
+        </header>
+        <div className="content">
+            <p>Share this link via</p>
+            <ul className="icons">
+                <a href="#" className='facebook'><Facebook/></a>
+                <a href="#" className='twitter'><Twitter/></a>
+                <a href="#" className='instagram'><Instagram/></a>
+                <a href="#" className='whatsapp'><WhatsApp/></a>
+                <a href="#" className='telegram'><Telegram/></a>
+            </ul>
+            <p>Or Copy Link</p>
+            <div className="field">
+                <LinkOutlined />
+                <input type="text" value='example.com/share-link'/>
+                <button><CopyAll /></button>
+            </div>
+        </div>
+
+    </div>
+  );
+}
